0762d84709d6a784d89e90fb7e235b1d97bd4c76
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-02 01:40:11 -0300

    feat: configurado tema da aplicação com chakra ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="description" content="Viagem pelo Mundo" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>World Trip</title>
-        <meta name="description" content="Viagem pelo Mundo" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
       </Head>
       <h1>World Trip</h1>
     </>
